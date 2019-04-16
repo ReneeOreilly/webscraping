1fb8883ef58a78cff6f19fe258c450a50b003706
ReneeOreilly <45985471+ReneeOreilly@users.noreply.github.com>
2019-04-15 19:13:46 -0500

    Add files via upload

--- Marsflask.py
+++ Marsflask.py
@@ -1,21 +1,38 @@
-import numpy as np
-import pandas as pd
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scrape_mars
 
-import datetime as dt
-
-import sqlalchemy
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
-
-
-from flask import Flask
+# Create an instance of Flask
 app = Flask(__name__)
 
-app.config["MONGO_URI"] = "mongodb://localhost:27017/craigslist_app"
-mongo = PyMongo(app)
+# Use PyMongo to establish Mongo connection
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
+
+# Route to render index.html template using data from Mongo
+@app.route("/")
+def home():
+
+    # Find one record of data from the mongo database
+    mars_mission = mongo.db.collection.find_one()
+
+    # Return template and data
+    return render_template("index.html", test=mars_mission)
+
+
+# Route that will trigger the scrape function
 @app.route("/scrape")
-mars_data = scrape_mars.scrape()
-listings.update({}, listings_data, upsert=True)
-return redirect("/", code=302)
+def scrape():
+
+    # Run the scrape function
+    mars_data = scrape_mars.scrape()
+
+    # Update the Mongo database using update and upsert=True
+    mongo.db.collection.update({}, mars_data, upsert=True)
+
+    # Redirect back to home page
+    return redirect("/")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
